90990935225181ddc469e760d213a03d5933adfd
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-12-31 03:42:36 +0530

    [Kavyasree J ganesh] Regex User Registration problem with Junit

--- src/main/java/com/bridgelabz/RegexMain.java
+++ src/main/java/com/bridgelabz/RegexMain.java
@@ -1,16 +1,44 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class RegexMain {
     public static void main(String[] args) {
-        Regex.validateFirstName("Kavya");
-        Regex.validateFirstName("kavya");
-        Regex.validateLastName("Ganesh");
-        Regex.validateLastName("ganesh");
-        Regex.validateEmail("kavyaganesh@gmail.com");
-        Regex.validateEmail("kavyaganeshgmail.com");
-        Regex.validateMobileNumber("91 9745775612");
-        Regex.validateMobileNumber("919745775612");
-        Regex.validatePassword("123");
-        Regex.validatePassword("kav&*j1234");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a Firstname");
+        String firstName = scan.nextLine();
+        Regex.validateFirstName(firstName);
+        System.out.println("Enter a Firstname");
+        firstName = scan.nextLine();
+        Regex.validateFirstName(firstName);
+
+        System.out.println("Enter a Lastname");
+        String lastName = scan.nextLine();
+        Regex.validateLastName(lastName);
+        System.out.println("Enter a Lastname");
+        lastName = scan.nextLine();
+        Regex.validateLastName(lastName);
+
+        System.out.println("Enter Email");
+        String email = scan.nextLine();
+        Regex.validateEmail(email);
+        System.out.println("Enter Email");
+        email = scan.nextLine();
+        Regex.validateEmail(email);
+
+        System.out.println("Enter Phone Number");
+        String phone = scan.nextLine();
+        Regex.validateMobileNumber(phone);
+        System.out.println("Enter Phone Number");
+        phone = scan.nextLine();
+        Regex.validateMobileNumber(phone);
+
+        System.out.println("Enter Password");
+        String password = scan.nextLine();
+        Regex.validatePassword(password);
+        System.out.println("Enter Password");
+        password = scan.nextLine();
+        Regex.validatePassword(password);
     }
 }
