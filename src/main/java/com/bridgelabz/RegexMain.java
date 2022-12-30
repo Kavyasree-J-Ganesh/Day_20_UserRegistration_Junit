e5bb8a81c4ba8f05fa8efb661fd8cd3c7b613aed
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-12-31 03:26:02 +0530

    [Kavyasree J ganesh] Regex User Registration problem with Junit

--- src/main/java/com/bridgelabz/RegexMain.java
+++ src/main/java/com/bridgelabz/RegexMain.java
@@ -1,17 +1,37 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class RegexMain {
     public static void main(String[] args) {
-        Regex.validateFirstName("Kavya");
-        Regex.validateFirstName("kavya");
-        System.out.println("");
+        Scanner scan = new Scanner(System.in);
 
-        Regex.validateLastName("Ganesh");
-        Regex.validateLastName("ganesh");
-        System.out.println("");
+        System.out.println("Enter a Firstname");
+        String firstName = scan.nextLine();
+        Regex.validateFirstName(firstName);
+        System.out.println("Enter a Firstname");
+        firstName = scan.nextLine();
+        Regex.validateFirstName(firstName);
 
-        Regex.validateEmail("kavyaganesh@gmail.com");
-        Regex.validateEmail("kavyaganeshgmail.com");
-        System.out.println("");
+        System.out.println("Enter a Lastname");
+        String lastName = scan.nextLine();
+        Regex.validateLastName(lastName);
+        System.out.println("Enter a Lastname");
+        lastName = scan.nextLine();
+        Regex.validateLastName(lastName);
+
+        System.out.println("Enter Email");
+        String email = scan.nextLine();
+        Regex.validateEmail(email);
+        System.out.println("Enter Email");
+        email = scan.nextLine();
+        Regex.validateEmail(email);
+
+        System.out.println("Enter Phone Number");
+        String phone = scan.nextLine();
+        Regex.validateMobileNumber(phone);
+        System.out.println("Enter Phone Number");
+        phone = scan.nextLine();
+        Regex.validateMobileNumber(phone);
     }
 }
