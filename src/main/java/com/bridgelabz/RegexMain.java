fe31f398d313a9aae7e0976f5dff9f3ed1ccef83
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-12-31 03:16:26 +0530

    [Kavyasree J ganesh] Regex User Registration problem with Junit

--- src/main/java/com/bridgelabz/RegexMain.java
+++ src/main/java/com/bridgelabz/RegexMain.java
@@ -1,8 +1,15 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class RegexMain {
     public static void main(String[] args) {
-        Regex.validateFirstName("Kavya");
-        Regex.validateFirstName("kavya");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a Firstname");
+        String firstName = scan.nextLine();
+        Regex.validateFirstName(firstName);
+        System.out.println("Enter a Firstname");
+        firstName = scan.nextLine();
+        Regex.validateFirstName(firstName);
     }
 }
